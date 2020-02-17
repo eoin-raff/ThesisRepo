303139f55063fd2937b73d2d55f65956223cb945
EoinRaff <eoinrafferty@gmail.com>
2020-02-17 15:43:08 +0100

    removed UE4.24 project

--- UE_PCG/Source/UE_PCG/UE_PCG.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "UE_PCG.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_PCG, "UE_PCG" );
