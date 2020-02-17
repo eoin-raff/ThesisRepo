d6cdf847df55cc64d149ab80bc8412b77b376318
EoinRaff <eoinrafferty@gmail.com>
2020-02-17 15:40:15 +0100

    Init UnityProject

---
+++ Unity_PCG/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.AutoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }   
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
