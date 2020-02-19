e6444edbe8b2a60631ece3e29f08404e1410ed56
EoinRaff <eoinrafferty@gmail.com>
2020-02-19 09:22:53 +0100

    Re-Init UE Project with RMC

--- UE_PCG_v4_23/Source/UE_PCG_v4_23/UE_PCG_v4_23.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "UE_PCG_v4_23.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_PCG_v4_23, "UE_PCG_v4_23" );
--- UE_PCG_v4_23/Source/UE_PCG_v4_23/UE_PCG_v4_23GameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "UE_PCG_v4_23GameModeBase.h"
-
