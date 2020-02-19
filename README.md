c5d9e5928d2c3f423cbd85c109f0579c58b1ece5
EoinRaff <eoinrafferty@gmail.com>
2020-02-19 09:04:24 +0100

    Update UE to 4.24, and installed RMC
    RMC **should** be working (it compiles now). Added steps and updated link to ReadMe.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ThesisRepo
+
+## Unreal Engine
+1. Install Runtime Mesh Compnent (RMC)
+1.1 Download .zip from github (for UE4.24, this [branch](https://github.com/gribuser/RuntimeMeshComponent) works)
+1.2 In your Project directory, create a Plugins folder if it does not exist already (note this is NOT the plugins folder for the engine version)
+1.3 Regenerate Visual Studio Project.
--- UE_PCG_v4_23/Source/UE_PCG_v4_23/UE_PCG_v4_23.Build.cs
+++ UE_PCG_v4_23/Source/UE_PCG_v4_23/UE_PCG_v4_23.Build.cs
@@ -9,7 +9,7 @@ public class UE_PCG_v4_23 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-		//PublicDependencyModuleNames.AddRange(new string[] { "RenderCore", "RHI", "RuntimeMeshComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "RenderCore", "RHI", "RuntimeMeshComponent" });
 		PublicDependencyModuleNames.AddRange(new string[] { "Landscape", "LandscapeEditor", "Foliage"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
