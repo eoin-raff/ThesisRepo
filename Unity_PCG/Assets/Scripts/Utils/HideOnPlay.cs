31c53a4a4c9571d785b51509bcafa3a4b6ebfdf2
EoinRaff <eoinrafferty@gmail.com>
2020-02-21 09:33:52 +0100

    Added HideOnPlay utility

---
+++ Unity_PCG/Assets/Scripts/Utils/HideOnPlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace MED10.Utilities
+{
+    public class HideOnPlay : MonoBehaviour
+    {
+        void Start()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
