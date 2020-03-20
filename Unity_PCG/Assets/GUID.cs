1b0feaeb5476a9b432f93efdb2bd5083ba72c6c8
Eoin Rafferty <eoinrafferty@gmail.com>
2020-03-20 09:08:24 +0100

    added basic GUID system

---
+++ Unity_PCG/Assets/GUID.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GUID : MonoBehaviour
+{
+    public TextMeshProUGUI display;
+    public String guid;
+    public void Generate()
+    {
+        guid = System.Guid.NewGuid().ToString();
+        display.text = guid;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
