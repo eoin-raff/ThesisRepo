0b548e3ec94f9681132e8968bc49779511a5ce03
Eoin Rafferty <eoinrafferty@gmail.com>
2020-03-20 11:15:00 +0100

    intial callback function to show server running

--- TelemetryServer/server.js
+++ TelemetryServer/server.js
@@ -4,4 +4,8 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+const port = 3000;
+
+app.listen(port, function(){
+  console.log("Server started on port " + port);
+});
