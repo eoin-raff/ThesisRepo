f20d28423c5466950fbaedeec4feee0405551d29
EoinRaff <eoinrafferty@gmail.com>
2020-03-23 09:58:24 +0100

    server test

--- TelemetryServer/server.js
+++ TelemetryServer/server.js
@@ -6,6 +6,15 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", function(req, res){
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/test", function(req, res){
+  res.send("<h1>test route</h1>");
+});
+
+
 app.listen(port, function(){
   console.log("Server started on port " + port);
 });
