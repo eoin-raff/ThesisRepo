b8d2df8558ad6cf11fffbf6905438ea34ffc20e2
EoinRaff <eoinrafferty@gmail.com>
2020-04-09 12:18:58 +0200

    updatedLobby and added initial sceneloader

---
+++ Unity_PCG/Assets/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using MED10.Architecture.Variables;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int sceneIndex;
+    public StringVariable displayText;
+
+    public void Load()
+    {
+        StartCoroutine(LoadSceneInBackground());
+    }
+    IEnumerator LoadSceneInBackground()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
