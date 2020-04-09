6f0403b8ddabaa7a032ab8d9adfded49bae7439b
EoinRaff <eoinrafferty@gmail.com>
2020-04-09 12:16:52 +0200

    added StringVariable and Reference

---
+++ Unity_PCG/Assets/Scripts/Architecture/Variables/StringVariable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MED10.Architecture.Variables
+{
+    [CreateAssetMenu]
+    public class StringVariable : ScriptableObject
+    {
+        public string Value;
+    }
+
+}
