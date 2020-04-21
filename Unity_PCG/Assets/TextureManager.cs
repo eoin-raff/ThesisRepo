5083dc22a86fa7e0d77f72718a4f23fb87d56e3b
EoinRaff <eoinrafferty@gmail.com>
2020-04-21 15:32:43 +0200

    Texture Manager to save files

---
+++ Unity_PCG/Assets/TextureManager.cs
@@ -0,0 +1,29 @@
+﻿using MED10.Architecture.Events;
+using MED10.Architecture.Variables;
+using MED10.PCG;
+using MED10.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureManager : MonoBehaviour
+{
+    public TerrainGenerator terrainGenerator;
+    //public StringVariable directory;
+    //public IntVariable seed;    
+    public string directory;
+    public int seed;
+    
+    public void SaveHeightmapTexture()
+    {
+        seed = terrainGenerator.Seed;
+        if (directory.Length <= 0)
+        {
+            directory = "Heightmaps_";// + DateTime.Today.ToString();        
+        }
+        string filename = "/HM_"+ seed;
+        Utils.BitmapToPNG(terrainGenerator.GetHeightMap(false), filename);
+    }
+
+}
