86134b36e7a28f9cef101c64a12c5c397712b5a7
Eoin Raff <eoinrafferty@gmail.com>
2020-04-28 15:49:30 +0200

    Made candidate struct and candidate evaluation function

--- Unity_PCG/Assets/Scripts/Narrative/NarrativeManager.cs
+++ Unity_PCG/Assets/Scripts/Narrative/NarrativeManager.cs
@@ -8,8 +8,7 @@ using System.Linq;
 
 public class NarrativeManager : MonoBehaviour
 {
-    public bool DEBUG = true;
-    public Camera playerCam;        // Add the camera from the player object to check if a possible location is in sight
+    public GameObject player;        // Add the camera from the player object to check if a possible location is in sight
 
     // Prefabs for staged areas - Should be designed first and then instatiated at a possible location
     public GameObject[] stagedAreas = new GameObject[5];
@@ -46,12 +45,12 @@ public class NarrativeManager : MonoBehaviour
             {
                 heightmap = terrainGenerator.GetHeightMap(false);
             }
-            Vector2 playerPos = new Vector2(playerCam.transform.position.x, playerCam.transform.position.z);
+            Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.z);
             if (Input.GetKeyDown(KeyCode.G))
             {
-                List<Vector2> candidates = PossibleSpawnPoints(playerPos, new Vector2(5, 5), 0, 1, 0, 1);
+                List<StagedAreaCandidatePosition> candidates = PossibleSpawnPoints(playerPos, new Vector2(5, 5), 0, 1, 0, 1);
 
-                if (FindBestPosition(candidates, out Vector2 spawnPosition))
+                if (FindBestPosition(candidates, new Vector4(5, 1, 1, 2).normalized, out Vector2 spawnPosition))
                 {
                     InstantiateStagedArea(spawnPosition);
                 }
@@ -62,7 +61,7 @@ public class NarrativeManager : MonoBehaviour
     {
         eventNum = 0;
 
-        positionAtLastSA = playerCam.transform.position;
+        positionAtLastSA = player.transform.position;
 
         float currentTime = Time.time;                  // Time when they start looking for SA spawn
         timeCoroutine = TimeManager(eventNum, currentTime);
@@ -72,7 +71,7 @@ public class NarrativeManager : MonoBehaviour
 
     private IEnumerator TimeManager(int num, float startTime)            // Manage time before next event has to spawn
     {
-        float distance = Vector3.Distance(playerCam.transform.position, positionAtLastSA);
+        float distance = Vector3.Distance(player.transform.position, positionAtLastSA);
 
         if (distance >= distanceBetweenSAs)
         {
@@ -129,23 +128,18 @@ public class NarrativeManager : MonoBehaviour
     }
 
 
-    private List<Vector2> PossibleSpawnPoints(Vector2 playerPosition, Vector2 stagedAreaSize, float minHeight, float maxHeight, float minSlope, float maxSlope)
+    private List<StagedAreaCandidatePosition> PossibleSpawnPoints(Vector2 playerPosition, Vector2 stagedAreaSize, float minHeight, float maxHeight, float minSlope, float maxSlope)
     {
         float targetHeight = minHeight + ((maxHeight - minHeight) / 2);
         float targetSlope = minSlope + ((maxSlope - minSlope) / 2);
 
-        int r = 15;
+        int r = 50;
         float[,] heightmap = terrainGenerator.GetHeightMap(false);
         int mappedY = (int)Utils.Map(playerPosition.x, 0, terrainGenerator.terrainData.size.x, 0, heightmap.GetLength(0));
         int mappedX = (int)Utils.Map(playerPosition.y, 0, terrainGenerator.terrainData.size.z, 0, heightmap.GetLength(1));
 
 
-        // Sorted dictionaries are not very performant, so it may be better to split into lists and sort manually
-        SortedDictionary<float, Vector2> positionsByScore = new SortedDictionary<float, Vector2>();
-
-        //float bestScore = float.MaxValue;
-        //Vector2 bestPosition = Vector2.zero; //Not currently used, SortedDict used instead
-        int validPoints = 0;
+        List<StagedAreaCandidatePosition> candidates = new List<StagedAreaCandidatePosition>();
 
         //Search the area around the player on the HM
         for (int y = Mathf.Max(0, mappedY - r); y < Mathf.Min(heightmap.GetLength(1), mappedY + r); y++)
@@ -155,43 +149,108 @@ public class NarrativeManager : MonoBehaviour
                 if (IsValidPoint(minHeight, maxHeight, minSlope, maxSlope, heightmap, y, x))
                 {
                     float totalScore = 0;
-                    validPoints++;
+                    float heightScore = 0;
+                    float slopeScore = 0;
+                    Vector2 pointPosition = new Vector2(x, y);
+
                     for (int ny = -(int)stagedAreaSize.y / 2; ny < (int)stagedAreaSize.y / 2; ny++)
                     {
                         for (int nx = -(int)stagedAreaSize.x / 2; nx < (int)stagedAreaSize.x / 2; nx++)
                         {
-                            totalScore += ScorePointValidity(x + nx, y + ny, heightmap, targetHeight, targetSlope);
+                            totalScore += ScorePointValidity(x + nx, y + ny, heightmap, targetHeight, targetSlope, out float h, out float s);
+                            heightScore += h;
+                            slopeScore += s;
                         }
                     }
-                    positionsByScore.Add(totalScore, new Vector2(x, y));
-                    //if (totalScore < bestScore)
-                    //{
-                    //    bestScore = totalScore;
-                    //    bestPosition = new Vector2(x, y);
-                    //}
+
+                    candidates.Add(new StagedAreaCandidatePosition
+                    {
+                        heightmapPosition = pointPosition,
+                        worldPosition = new Vector3(
+                            y / (float)terrainGenerator.terrainData.heightmapResolution * terrainGenerator.terrainData.size.z,
+                            heightmap[(int)x, (int)y] * terrainGenerator.terrainData.size.y,
+                            x / (float)terrainGenerator.terrainData.heightmapResolution * terrainGenerator.terrainData.size.x
+                        ),
+                        heightScore = heightScore,
+                        slopeScore = slopeScore,
+                        dstFromPlayer = Vector2.Distance(playerPosition, pointPosition)
+                    });
                 }
             }
         }
-        //TODO: Run profiler on this, Linq can be expensive to run.
-        List<Vector2> possiblePlaces = positionsByScore.Values.ToList();
-
-
 
         // Search in an area of r radius from the position denoted by x and y which is the current player position
         // Find first 10 suitable areas that fulfill scale, height, and slope
         // Make sure a found area can't be found again
         // Return up to 10 areas where spawning can happen
 
-        return possiblePlaces;               // Return center of found area
+        return candidates;
     }
 
-    private bool FindBestPosition(List<Vector2> candidates, out Vector2 position)
+    private bool FindBestPosition(List<StagedAreaCandidatePosition> candidates, Vector4 weights, out Vector2 position)
     {
         // candidates[0] is best fit in terms of height and slope, but this function should take other parameters such as distance and direction etc. into account
         // for now, lets just use [0]
+
         if (candidates.Count > 0)
         {
-            position = candidates[0];
+            float bestScore = float.MaxValue;
+            StagedAreaCandidatePosition chosenCandidate = new StagedAreaCandidatePosition
+            {
+                heightmapPosition = Vector2.zero
+            };
+            for (int i = 0; i < candidates.Count; i++)
+            {
+                //Evaluate Candidates
+                //                float directionToPlayer = Vector2.Dot((player.transform.position.XZ() - candidates[i].position).normalized, player.transform.forward.normalized);
+                Vector3 directionToCandidate = (candidates[i].worldPosition - player.transform.position);
+                float directionScore = Vector3.Dot(player.transform.forward.normalized, directionToCandidate.normalized);
+
+                if (Mathf.Abs(directionScore) > 0.75f) //Area to ignore infront and behind you
+                {
+                    //ignore points behind you
+                    Debug.DrawRay(player.transform.position, directionToCandidate, Color.red, 10f);
+
+                    continue;
+                }
+                Debug.DrawRay(player.transform.position, directionToCandidate, Color.blue, 10f);
+
+                float totalScore = (
+                    (directionScore * weights.x) //Lower score when far from centre of view
+                    + (candidates[i].heightScore * weights.y) //lower when closer to target
+                    + (candidates[i].slopeScore * weights.z)  //lower when closer to target
+                    + 1 - ((candidates[i].dstFromPlayer * -weights.w))
+                    ) / 4;
+                if (totalScore < bestScore)
+                {
+                    bestScore = totalScore;
+                    chosenCandidate = candidates[i];
+                    Debug.DrawRay(player.transform.position, directionToCandidate, Color.green, 1f);
+
+                }
+
+
+            }
+            Vector3 dir = (chosenCandidate.worldPosition - player.transform.position);
+            Debug.DrawRay(player.transform.position, dir, Color.cyan, 15f);
+            //Debug.DrawRay(player.transform.position, player.transform.forward, Color.yellow, 15f);
+
+
+            //Debug.Log(directionScore);
+            position = chosenCandidate.heightmapPosition;
+            //Debug.Log(Vector2.Dot((player.transform.position.XZ() - candidates[0].position).normalized, player.transform.forward.XZ().normalized));
+
+            //Debug.DrawRay(player.transform.position, new Vector3(
+            //    player.transform.forward.XZ().normalized.x, 
+            //    player.transform.position.y, 
+            //    player.transform.forward.XZ().normalized.y) * 10, 
+            //    Color.red, 10f);
+
+            //Debug.DrawRay(player.transform.position, new Vector3(
+            //    (candidates[0].position.y - player.transform.position.x),
+            //    player.transform.position.y,
+            //    (candidates[0].position.x - player.transform.position.y)) * 10, Color.blue, 10f);
+
             return true;
         }
         Debug.LogWarning("No Suitable Candidates for staged area.", this);
@@ -220,17 +279,17 @@ public class NarrativeManager : MonoBehaviour
         return isInHeightRange && isInSlopeRange;
     }
 
-    private float ScorePointValidity(int x, int y, float[,] hm, float targetHeight, float targetSlope)
+    private float ScorePointValidity(int x, int y, float[,] hm, float targetHeight, float targetSlope, out float heightScore, out float slopeScore)
     {
         float height = hm[x, y];
         float slope = terrainGenerator.terrainData.GetSteepness(
             x / (float)terrainGenerator.terrainData.alphamapResolution,
             y / (float)terrainGenerator.terrainData.alphamapResolution);
 
-        float score_h = Mathf.Abs(targetHeight - height);
-        float score_s = Mathf.Abs(targetSlope - slope);
+        heightScore = Mathf.Abs(targetHeight - height);
+        slopeScore = Mathf.Abs(targetSlope - slope);
 
-        return score_h + score_s;
+        return heightScore + slopeScore;
     }
 
     private void CinematicSequence(Transform target)
@@ -241,9 +300,9 @@ public class NarrativeManager : MonoBehaviour
 
         float speed = 5.0f;                 // this is the speed at which the camera moves
 
-        playerCam.transform.position = Vector3.Lerp(transform.position, lookAtPosition, speed);
+        player.transform.position = Vector3.Lerp(transform.position, lookAtPosition, speed);
 
-        playerCam.transform.LookAt(lookAtPosition);
+        player.transform.LookAt(lookAtPosition);
 
         LookAtSA(5.0f);                     // Stare at the SA before moving again
 
@@ -254,10 +313,10 @@ public class NarrativeManager : MonoBehaviour
     private IEnumerator WaitToStartCinematic(Transform locationOfSA)        // Check if the player is close enough to the SA to start the cinematic sequence
     {
 
-        if (playerCam.transform.position.x >= locationOfSA.position.x - withinSA &&
-            playerCam.transform.position.x <= locationOfSA.position.x + withinSA &&
-            playerCam.transform.position.y >= locationOfSA.position.y - withinSA &&
-            playerCam.transform.position.y <= locationOfSA.position.y + withinSA)
+        if (player.transform.position.x >= locationOfSA.position.x - withinSA &&
+            player.transform.position.x <= locationOfSA.position.x + withinSA &&
+            player.transform.position.y >= locationOfSA.position.y - withinSA &&
+            player.transform.position.y <= locationOfSA.position.y + withinSA)
         {
             CinematicSequence(locationOfSA);
             yield return new WaitForSeconds(1.0f);
@@ -286,4 +345,14 @@ public class SAParameters : TableItem
     public float maxSlope = 1.0f;
     public float minHeight = 0.0f;
     public float maxHeight = 1.0f;
+}
+
+[Serializable]
+public struct StagedAreaCandidatePosition
+{
+    public Vector2 heightmapPosition;
+    public Vector3 worldPosition;
+    public float heightScore;
+    public float slopeScore;
+    public float dstFromPlayer;
 }
