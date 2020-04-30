eb386f627798896d991088fecef57ba0896a4fdf
EoinRaff <eoinrafferty@gmail.com>
2020-04-30 11:13:01 +0200

    Added Singleton architecture

---
+++ Unity_PCG/Assets/Scripts/Architecture/Singleton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MED10.Architecture
+{
+    /// <summary>
+    /// The singeton abstract class is designed to be inherited by any object which wants singleton functionality.
+    /// </summary>
+    /// <typeparam name="Type">The type of class to function as a singleton. e.g. public class GameManager : Singleton<GameManager></typeparam>
+    public abstract class Singleton<Type> : MonoBehaviour where Type : MonoBehaviour
+    {
+        private static Type instance;
+
+        /// <summary>
+        /// Lazy instantiation of singleton object
+        /// </summary>
+        /// <returns></returns>
+        public static Type Instance()
+        {
+            if (instance == null)
+            {
+                GameObject singletonObject = new GameObject();
+                Type type = singletonObject.AddComponent<Type>();
+                singletonObject.name = type.name;
+                singletonObject.transform.position = Vector3.zero;
+                singletonObject.transform.rotation = Quaternion.identity;
+                return type;
+            }
+            else
+            {
+                return instance;
+            }
+        }
+        protected void Awake()
+        {
+            if (instance == null)
+            {
+                instance = gameObject.GetComponent<Type>();
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    } 
+}
