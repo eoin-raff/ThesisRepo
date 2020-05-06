ea5953dbf027526b896cd57fa3cf9faef64bdeb5
EoinRaff <eoinrafferty@gmail.com>
2020-05-06 13:41:50 +0200

    Set variableObject<T> base class and fixedValue for resetting variables

---
+++ Unity_PCG/Assets/Scripts/Architecture/Variables/VariableObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace MED10.Architecture.Variables
+{
+	[CreateAssetMenu]
+	public abstract class VariableObject<T> : ScriptableObject
+	{
+		public T Value;
+		[SerializeField]
+		private T FixedValue;
+		private void Awake()
+		{
+			Value = FixedValue;
+		}
+	}
+}
