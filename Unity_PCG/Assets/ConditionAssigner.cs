508529059dbcecc614699683b02a56fae340223a
ThomasLund17 <thomas.lund17@gmail.com>
2020-05-07 15:09:57 +0200

    Added intro and outro splash screens + send to forms capabilities

---
+++ Unity_PCG/Assets/ConditionAssigner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConditionAssigner : MonoBehaviour
+{
+    public int condition;
+    public GameObject showCon;
+    public GameObject backgroundImg, text1, text2;
+
+    public void AssignCondition()
+    {
+        float rnd = Random.value;
+
+        if (rnd > 0.5f)
+        {
+            condition = 1;
+            showCon.GetComponent<Text>().text = ("Condition: 1 : " + rnd);
+        }
+        else
+        {
+            condition = 0;
+            showCon.GetComponent<Text>().text = ("Condition: 0 : " + rnd);
+        }
+    }
+
+    public void Fader()
+    {
+        backgroundImg.GetComponent<Image>().CrossFadeAlpha(0.0f, 2.0f, false);
+        text1.GetComponent<Text>().CrossFadeAlpha(0.0f, 1.0f, false);
+        text2.GetComponent<Text>().CrossFadeAlpha(0.0f, 1.0f, false);
+    }
+}
---
+++ Unity_PCG/Assets/SendToGoogle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendToGoogle : MonoBehaviour
+{
+    public void SendToForms()
+    {
+        int seed = 1;       // Get reference to seed here
+
+        string seedString = seed.ToString();
+
+        if (gameObject.GetComponent<ConditionAssigner>().condition == 1)
+        {
+            Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLScys2WbSD_cEcWUDwip8UyAzHrvdOk4AHFwhibrr8sDPqjn_Q/viewform?usp=pp_url&entry.594058135=Condition+A&entry.150889762=" + seedString);
+
+        }
+        else
+        {
+            Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLScys2WbSD_cEcWUDwip8UyAzHrvdOk4AHFwhibrr8sDPqjn_Q/viewform?usp=pp_url&entry.594058135=Condition+B&entry.150889762=" + seedString);
+        }
+    }
+}
